717ee825bcba5a732c51952932735a03cafd737e
Hikmat Huseynov <hikmet.huseynov@ferrumcapital.az>
2022-10-14 17:58:55 +0400

    navbar

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,10 @@ import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <>
       <Navbar />
       {children}
-    </div>
+    </>
   );
 };
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import Image from "next/image";
+import logo from "../public/assets/images/logo.svg";
+import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+      <nav className="w-full">
+        <div className="p-10 flex justify-between items-center">
+          <div className={`${styles.burger} mr-5`}>
+            <div className={styles.bar}></div>
+            <div className={styles.bar}></div>
+            <div className={styles.bar}></div>
+          </div>
+          <div className="logo">
+            <Image src={logo} alt="logo"/>
+          </div>
+          <ul className="navigation flex mr-auto hidden lg:flex">
+            <li className="nav-item text-sm xl:text-base px-3 xl:px-4">Men wear</li>
+            <li className="nav-item text-sm xl:text-base px-3 xl:px-4">Women wear</li>
+            <li className="nav-item text-sm xl:text-base px-3 xl:px-4">Search</li>
+            <li className="nav-item text-sm xl:text-base px-3 xl:px-4">Shops</li>
+          </ul>
+            <div className="language">
+              English
+            </div>
+          <div className="navbar-right flex">
+            <div className="search">
+              <i>search</i>
+            </div>
+            <div className="sign-in">
+              Sign-in
+            </div>
+            <div className="basket">
+              <i>basket</i>
+            </div>
+          </div>
+        </div>
+      </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
